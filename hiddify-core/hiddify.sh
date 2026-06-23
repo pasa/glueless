512271e512705139e3b55e83d20ef9ed9aee7ee5
pasa <parilinsa@gmail.com>
2026-06-23 14:19:54 +0300

    trying hard 5

--- hiddify-core/hiddify.sh
+++ hiddify-core/hiddify.sh
@@ -97,7 +97,7 @@ trap cleanup TERM INT
 log_message "Setting up logging infrastructure..."
 setup_log_pipes
 
-ls /hiddify/ | log_pipe
+ls -l /hiddify/ | log_pipe
 
 /hiddify/HiddifyCli version | log_pipe
 
